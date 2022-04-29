353fba7edeebfaebb5eb9ceccd20196db7cb87cb
Gahkkkj <gabrieldasilvapereira300@outlook.com>
2022-04-28 21:52:24 -0300

    perfil (terminando)

--- perfil.php
+++ perfil.php
@@ -13,32 +13,35 @@
 </head>
 
 <body class="perfil-body">
-
-    <img src="imagens/mulher.png" class="circle">
-
-
-    <section class="mb-3">
-        <div class="d-flex flex-row bd-highlight mb-3">
-            <button type="button" class="btn btn-light mr-3 p-2 btn-lg pequeno">Pedidos</button>
-            <br>
-            <button type="button" class="btn btn-light mr-3 p-2 btn-lg pequeno">Reenbolsar</button>
-            <br>
+    <div>
+        <img src="imagens/mulher.png" class="circle">
+    </div>
+    <section>
+        <div class="row justify-content-center  align-items-center">
+            <div class="bd-highlight mb-3">
+                <button type="button" class="btn btn-light mr-3 p-2 btn-lg pequeno">Pedidos</button>
+                <br>
+                <button type="button" class="btn btn-light mr-3 p-2 btn-lg pequeno">Reenbolsar</button>
+                <br>
+            </div>
+           
+            <div class="bd-highlight mb-3">
+                <button type="button" class="btn btn-light btn-lg mr-3 p-2 pequeno">Favoritos</button>
+                <br>
+                <button type="button" class="btn btn-light btn-lg mr-3 p-2 pequeno">Avaliar</button>
+                <br>
+            </div>
+            <div class="row">
+                <button type="button" class="btn btn-light btn-lg d-block grande">Alterar dados</button>
+                <br>
+                <button type="button" class="btn btn-light btn-lg d-block grande">Avisos</button>
+                <br>
+                <button type="button" class="btn btn-light btn-lg d-block grande">Formas de pagamento</button>
+                <br>
+                <button type="button" class="btn btn-light btn-lg d-block grande">Sair</button>
+                <br>
+            </div>
         </div>
-        <div class="d-flex flex-row bd-highlight mb-3">
-        <button type="button" class="btn btn-light btn-lg mr-3 p-2 pequeno">Favoritos</button>
-        <br>
-        <button type="button" class="btn btn-light btn-lg mr-3 p-2 pequeno">Avaliar</button>
-        <br>
-        </div>
-        <button type="button" class="btn btn-light btn-lg btn-block grande">Alterar dados</button>
-        <br>
-        <button type="button" class="btn btn-light btn-lg btn-block grande">Avisos</button>
-        <br>
-        <button type="button" class="btn btn-light btn-lg btn-block grande">Formas de pagamento</button>
-        <br>
-        <button type="button" class="btn btn-light btn-lg btn-block grande">Sair</button>
-        <br>
-
     </section>
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
