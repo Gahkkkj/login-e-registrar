d3fb9470e39e21fb321c154df6cc193ee8ebd1f8
Gahkkkj <101150465+Gahkkkj@users.noreply.github.com>
2022-05-23 20:33:38 -0300

    final

--- Login.php
+++ Login.php
@@ -9,43 +9,45 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
   <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
-  <link rel="stylesheet" href="./CSS/styles.css">
+  <link rel="stylesheet" href="./assents/CSS/login.css">
   <title>login</title>
 </head>
 
-<body class="bodypage1">
-  <div class="container m-auto text-light">
-    <div class="container">
-      <H1>Login</h1>
+<body>
+  <section>
 
-      <i class='fab fa-facebook' style='font-size:48px;'></i>
-      <i class='fab fa-twitter-square' style='font-size:48px;'></i>
-      <i class='fab fa-google-plus' style='font-size:48px;'></i>
-
-    </div>
-
-    <section class="formulario">
-      <form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-
+    <div class="container-login100">
+      <div class="wrap-login100">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="card">
+                <form onsubmit="event.preventDefault()" class="box">
+                  <h1>Login</h1>
+                  <p class="text-muted"> Por favor coloque seu login e senha!</p>
+                  <input type="text" name="" placeholder="Nome de Usuario">
+                  <input type="password" name="" placeholder="Senha">
+                  <a class="forgot text-muted" href="#">Esqueceu senha?</a><br>
+                  <a class="forgot text-muted" href="./Cadastrar.php">Cadastre-se</a>
+                  <input type="submit" name="" value="Login" href="#">
+                  <div class="col-md-12">
+                    <ul class="social-network social-circle">
+                      <li><a href="#" class="icoFacebook" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                      <li><a href="#" class="icoTwitter" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+                      <li><a href="#" class="icoGoogle" title="Google +"><i class="fab fa-google-plus"></i></a></li>
+                    </ul>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
+  </section>
 
-        <button type="login" class="btn btn-primary">login</button>
-      </form>
-
-
-      <a href="Cadastrar.php" class="btn btn-outline-dark">Não é cadastradp ainda? cadastre-se aqui</a>
-
-  </div>
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
-  <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+  <script src="assents/JS/login"></script>
+  </div>
 </body>
 
 </html>
--- perfil.php
+++ perfil.php
@@ -1,23 +1,53 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/perfilstyles.css">
+    <title>Perfil</title>
+</head>
 
-    <title>perfil</title>
-  </head>
-  <body>
-    
-
-
-
+<body class="perfil-body">
+    <div>
+        <img src="imagens/mulher.png" class="circle">
+    </div>
+    <section>
+        <div class="row justify-content-center  align-items-center">
+            <div class="bd-highlight mb-3">
+                <button type="button" class="btn btn-light mr-3 p-2 btn-lg pequeno">Pedidos</button>
+                <br>
+                <button type="button" class="btn btn-light mr-3 p-2 btn-lg pequeno">Reenbolsar</button>
+                <br>
+            </div>
+           
+            <div class="bd-highlight mb-3">
+                <button type="button" class="btn btn-light btn-lg mr-3 p-2 pequeno">Favoritos</button>
+                <br>
+                <button type="button" class="btn btn-light btn-lg mr-3 p-2 pequeno">Avaliar</button>
+                <br>
+            </div>
+            <div class="row">
+                <button type="button" class="btn btn-light btn-lg d-block grande">Alterar dados</button>
+                <br>
+                <button type="button" class="btn btn-light btn-lg d-block grande">Avisos</button>
+                <br>
+                <button type="button" class="btn btn-light btn-lg d-block grande">Formas de pagamento</button>
+                <br>
+                <button type="button" class="btn btn-light btn-lg d-block grande">Sair</button>
+                <br>
+            </div>
+        </div>
+    </section>
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
->   <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
-  </body>
+
+
+</body>
+
 </html>
